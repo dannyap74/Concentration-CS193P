668c408cfa70fa994ae56ff02a4e7e3e1567c3b4
dannyap74 <danny.paleyev@gmail.com>
2020-01-02 21:31:12 +0500

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Даниил Палеев on 02.01.2020.
+//  Copyright © 2020 Даниил Палеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
